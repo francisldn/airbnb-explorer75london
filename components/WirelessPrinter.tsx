1b80871886c2c3e6415caadb37d5b418bf36e346
Francis_ldn <fnhh04@gmail.com>
2022-07-10 14:40:09 +0100

    edit linting

--- components/WirelessPrinter.tsx
+++ components/WirelessPrinter.tsx
@@ -20,14 +20,14 @@ const WirelessPrinter = () => {
                                 <li>To use the printer from your <b>mobile phone</b>, you just need to connect to the same local wifi network.</li>
                                     <li className="ml-5">Refer to the <span className="cursor-pointer text-blue-600 text-underline" onClick={() => router.reload('../pages/Usermanual.tsx')}>Wifi</span> page to learn more.</li>
                                 <li>You may find it useful to watch the video below:</li>
-                                    <li className="list-none"><iframe width="300" height="200" src="https://www.youtube.com/embed/xwRFoe_yMt4" title="Xerox® B205/B210/B215 Mobile Print for iPhone and iPad" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+                                    <li className="list-none"><iframe width="300" height="200" src="https://www.youtube.com/embed/xwRFoe_yMt4" title="Xerox® B205/B210/B215 Mobile Print for iPhone and iPad" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></li>
                                 <li>To use the printer from your <b>laptop</b>, you will need to first install the printer driver. Refer to the video below:</li>
                                 <li className="flex gap-12 ml-5">
                                     <li className="list-none"><b>MacOS</b>
-                                        <li className="list-none"><iframe width="300" height="200" src="https://www.youtube.com/embed/TxIYuWIwZCA" title="Xerox® B215 Print Drivers Install for Apple IOS" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+                                        <li className="list-none"><iframe width="300" height="200" src="https://www.youtube.com/embed/TxIYuWIwZCA" title="Xerox® B215 Print Drivers Install for Apple IOS" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></li>
                                     </li>
                                     <li className="list-none"><b>Windows</b>
-                                        <li className="list-none"><iframe width="300" height="200" src="https://www.youtube.com/embed/ssouuYGaHVs" title="Xerox® B215 Print Drivers Install for Windows" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+                                        <li className="list-none"><iframe width="300" height="200" src="https://www.youtube.com/embed/ssouuYGaHVs" title="Xerox® B215 Print Drivers Install for Windows" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe></li>
                                     </li>
                                 </li>
                             </ul>
