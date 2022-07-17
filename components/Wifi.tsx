db4c4d17c6a0b87c3227d9dee11ae39604ae7005
Francis_ldn <fnhh04@gmail.com>
2022-07-17 11:35:47 +0100

    add wifi qr code

--- components/Wifi.tsx
+++ components/Wifi.tsx
@@ -11,19 +11,12 @@ const Wifi = () => {
                         <li><strong>Location:</strong> Wifi router is located on the side of the entrance hallway, beside the bathroom door</li>
                         <li>Wifi password can be found on the wifi router, as shown in the picture</li>
                         <li><strong>How to connect:</strong>
-                            <ul className="flex gap-8">
-                                <li className="w-[300px] h-[300px] mt-3 overflow-hidden"><img src="wifiname.jpg" alt="wifi-name"/></li>
-                                <ul className="list-disc leading-8">
-                                    <li>Look for the Wifi names as shown in the picture on the left</li>
-                                    <li>You can connect to either:
-                                        <ul className="list-disc ml-6">
-                                            <li>FCFB Hyperoptic 1Gb Fibre 5Ghz</li>
-                                            <li>FCFB Hyperoptic 1Gb Fibre 2.4Ghz</li>
-                                        </ul>
-                                    </li>
-                                    <li>Enter the wifi password as written on the router</li>
-                                </ul>
-                                
+                            <ul className="ml-5 list-disc pt-2">
+                                <li>Easiest way: Scan this QR code with your phone camera to connect to the wifi. You don't need to enter password this way.</li>
+                                <div className="w-[200px] ml-5">
+                                    <img src="qrcode.png" alt="wifi qr-code" className="object-contain w-full h-full" />
+                                </div>
+                                <li>Alternatively, you can also look for the wifi name: <b>FCFB Hyperoptic 1Gb Fibre 5Ghz</b>, <br />and then enter the password on the wifi router</li>
                             </ul>
                         </li>
 
